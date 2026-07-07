987df535dab29751b941b300869b5ffb306ca9bd
Ankita <ankita180206@gmail.com>
2026-07-07 13:30:23 +0530

    Create members.sql

---
+++ members.sql
@@ -0,0 +1,12 @@
+CREATE TABLE MEMBERS(
+     memberId VARCHAR2(10) PRIMARY KEY NOT NULL,
+     name VARCHAR2(20) NOT NULL,
+     DOB VARCHAR2(10),
+     gender VARCHAR2(6),
+     phoneNo VARCHAR2(10),
+     email VARCHAR2(30),
+     address VARCHAR2(50),
+     city VARCHAR2(20),
+     regDate VARCHAR2(10),
+     status VARCHAR2(10) CHECK (status = 'active' OR  status = 'inactive') NOT NULL
+);
